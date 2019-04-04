cec34b85c78b8f6c327f4e2383b91a5a36bd7703
alex <alex@staticlibs.net>
2019-04-04 22:55:53 +0100

    1.7.4 docs

--- docs/html/search/files_75.js
+++ docs/html/search/files_75.js
@@ -1,6 +1,7 @@
 var searchData=
 [
   ['url_5futils_2ehpp',['url_utils.hpp',['../url__utils_8hpp.html',1,'']]],
+  ['user_5futils_2ehpp',['user_utils.hpp',['../user__utils_8hpp.html',1,'']]],
   ['utils_2ehpp',['utils.hpp',['../utils_8hpp.html',1,'']]],
   ['utils_5fexception_2ehpp',['utils_exception.hpp',['../utils__exception_8hpp.html',1,'']]]
 ];
