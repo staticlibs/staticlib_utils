4c340710a135a0ba4ad97c446d78e190415d9f57
alex <alex@staticlibs.net>
2019-04-04 23:02:45 +0100

    header fix

--- src/signal_utils.cpp
+++ src/signal_utils.cpp
@@ -33,9 +33,7 @@
 
 #include "staticlib/config.hpp"
 #ifdef STATICLIB_WINDOWS
-#define WIN32_LEAN_AND_MEAN
-#define NOMINMAX
-#include <windows.h>
+#include "staticlib/support/windows.hpp"
 #endif // STATICLIB_WINDOWS
 
 namespace staticlib {
